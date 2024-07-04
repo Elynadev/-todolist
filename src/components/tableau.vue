4f26e5e16ebfc15e824228675c46089e27e1256f
arnaudsonkpianCLEAR <arnaudsonkpian205@gmail.com>
2024-07-04 18:46:06 +0200

    modifié

--- src/components/tableau.vue
+++ src/components/tableau.vue
@@ -1,15 +1,23 @@
 <template>
   <h1>tableau</h1>
+  <div class="tab">
   <table>
     <thead>
         <tr>
-            <th>tâche</th>
-            <th>ajouter </th>
-            <th>modifier </th>
+            <th>Task</th>
+            <th>Status </th>
+            <th>comment </th>
             <th>etat</th>
+           
         </tr>
     </thead>
     <tbody>
+        <tr>
+            <td>nom</td>
+            <td><input type="checkbox">traité</td>            
+            <td><button>supprimer</button><button>modifier</button></td>
+            <td><input type="checkbox">confirmé</td>
+        </tr>
         <tr>
             <td></td>
             <td></td>
@@ -18,14 +26,27 @@
         </tr>
     </tbody>
   </table>
+</div>
 </template>
 
 <script setup>
+
 </script>
 
 
 
 <style scoped>
+th,tr,td{
+    /* border-collapse: collapse; */
+    border: 1px solid black;
+    line-height: 100px;
+    width: 200px;
+}
+.tab{
+    display: flex;
+    align-content: center;
+
+}
 
 </style>
 
